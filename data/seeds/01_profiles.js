f453579aa78c5fac8869190615534a5164b296ca
Carlos MANNIE Alvarez-Berrios <carlos.alvarezberrio@gmail.com>
2021-01-20 23:47:27 -0500

    Seed: Ran seeds for profiles, stores, and products

--- data/seeds/01_profiles.js
+++ data/seeds/01_profiles.js
@@ -3,7 +3,7 @@ const faker = require('faker');
 const profiles = [...new Array(5)].map((i, idx) => ({
   okta_id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
   avatarUrl: faker.image.avatar(),
-  email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
+  email: idx === 0 ? 'llama001@maildrop.cc' : faker.internet.email(),
   name:
     idx === 0
       ? 'Test001 User'
